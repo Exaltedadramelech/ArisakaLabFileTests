e61b0f33fe1d9f6a64fb0c46d3205edeac726a74
Exaltedadramelech <Exaltedadramelech@gmail.com>
2018-12-03 20:01:01 -0800

    Add files via upload

--- AnstisExp.py
+++ AnstisExp.py
@@ -43,6 +43,7 @@ def letterrandomizer():
 	l="L"
 	m="M"
 	n="N"
+	o="O"
 	p="P"
 	q="Q"
 	r="R"
@@ -56,57 +57,83 @@ def letterrandomizer():
 	z="Z"
 	numbertoletterperm=numbertoletter
 	if numbertoletterperm==1:
-		return "A"
+		A=numbertoletter
+		return a
 	elif numbertoletterperm==2:
-		return "B"
+		B=numbertoletter
+		return b
 	elif numbertoletter==3:
-		return "C"
+		C=numbertoletter
+		return c
 	elif numbertoletter==4:
-		return "D"
+		D=numbertoletter
+		return d
 	elif numbertoletter==5:
-		return "E"
+		E=numbertoletter
+		return e
 	elif numbertoletter==6:
-		return "F"
+		F=numbertoletter
+		return f
 	elif numbertoletter==7:
-		return "G"
+		G=numbertoletter
+		return g
 	elif numbertoletter==8:
-		return "H"
+		H=numbertoletter
+		return h
 	elif numbertoletter==9:
-		return "I"
+		I=numbertoletter
+		return i
 	elif numbertoletter==10:
-		return "J"
+		J=numbertoletter
+		return j
 	elif numbertoletter==11:
-		return "K"
+		K=numbertoletter
+		return k
 	elif numbertoletter==12:
-		return "L"
+		L=numbertoletter
+		return l
 	elif numbertoletter==13:
-		return "M"
+		M=numbertoletter
+		return m
 	elif numbertoletter==14:
-		return "N"
+		N=numbertoletter
+		return n
 	elif numbertoletter==15:
-		return "O"
+		O=numbertoletter
+		return o
 	elif numbertoletter==16:
-		return "P"
+		P=numbertoletter
+		return p
 	elif numbertoletter==17:
-		return "Q"
+		Q=numbertoletter
+		return q
 	elif numbertoletter==18:
-		return "R"
+		R=numbertoletter
+		return r
 	elif numbertoletter==19:
-		return "S"
+		S=numbertoletter
+		return s
 	elif numbertoletter==20:
-		return "T"
+		T=numbertoletter
+		return t
 	elif numbertoletter==21:
-		return "U"
+		U=numbertoletter
+		return u
 	elif numbertoletter==22:
-		return "V"
+		V=numbertoletter
+		return v
 	elif numbertoletter==23:
-		return "W"
+		W=numbertoletter
+		return w
 	elif numbertoletter==24:
-		return "X"
+		X=numbertoletter
+		return x
 	elif numbertoletter==25:
-		return "Y"
+		Y=numbertoletter
+		return y
 	elif numbertoletter==26:
-		return "Z"
+		Z=numbertoletter
+		return z
 
 
 
@@ -129,32 +156,54 @@ def lettertext(text1, text2, text3, text4, text5, text6, text7, text8 ,h, posi):
 	six.draw()
 	seven.draw()
 	eight.draw()
-	print text1
-	print text2
-	print text3
 
 Instructions1=visual.TextStim(win=window, text='Record the letters and position of the letters that you can recall, then press the spacebar when ready to see the actual configuration', pos=(0,0))
 
+def letterring():
+	lettertext("A","B","C","D","E","F","G","H",2.0,5.0)
+	lettertext("A","B","C","D","E","F","G","H",2.0,5.0)
+	lettertext("A","B","C","D","E","F","G","H",2.0,5.0)
+	lettertext("A","B","C","D","E","F","G","H",2.0,5.0)
+
+
 def letterfunc():
 	count=0
-	while clock.getTime()<=15.0:
-		if clock.getTime()<5.0:
+	countspace=0
+	countinstruct=0
+	while clock.getTime()<=20.0:
+		print clock.getTime()
+		if clock.getTime()<3.0:
 			vline.draw()
 			hline.draw()
 			window.flip()
+		elif event.getKeys("space"):
+			print "Fuck"
+			break
 		elif clock.getTime()>=5.0 and clock.getTime()<=10.0:
+			Instructions1.draw()
+			window.flip()
+		elif clock.getTime()>=3.0 and clock.getTime()<=5.0:
 			if count==0:
-				lettertext(letterrandomizer(),letterrandomizer(),letterrandomizer(),letterrandomizer(),letterrandomizer(),letterrandomizer(),letterrandomizer(),letterrandomizer(),1.0,1.0)
+				ring1=lettertext(letterrandomizer(),letterrandomizer(),letterrandomizer(),letterrandomizer(),letterrandomizer(),letterrandomizer(),letterrandomizer(),letterrandomizer(),1.0,1.0)
 				#lettertext("H","G","F","E","D","C","B","A",2.0,3.0)
 				#lettertext("A","B","C","D","E","F","G","H",3.0,5.0)
-				lettertext("A","B","C","D","E","F","G","H",2.0,5.0)
+				ring2=lettertext("A","B","C","D","E","F","G","H",2.0,5.0)
+				ring1perm=ring1
+				ring2perm=ring2
+
 				window.flip()
 				count=1
 			elif count>=1:
-				count=1
-		elif clock.getTime()>10.0:
-			Instructions1.draw()
+				count=1 
+		elif clock.getTime()>=10.0 and clock.getTime()<20.0:
+			#if ring2perm==lettertext("A","B","C","D","E","F","G","H",2.0,5.0):
+				#print "Kill"
+			ring1()
+			ring2()
 			window.flip()
+
+				
+
 			#if clock.getTime()>=20.0 and clock.getTime()<=30.0:
 			#	clock.getTime()=11.0
 	#		elif event.getKeys('space'):
