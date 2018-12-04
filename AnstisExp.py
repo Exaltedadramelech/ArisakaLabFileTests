ad920f9124701f8916fe7c517933aa0a519898f4
Exaltedadramelech <Exaltedadramelech@gmail.com>
2018-12-03 21:08:26 -0800

    Add files via upload
    Hopefully the final version of the randomized letter portion of this experiment... If you need to add more letterrings just copy paste the array building function and change its label and its outputs label.

--- AnstisExp.py
+++ AnstisExp.py
@@ -27,117 +27,242 @@ clock = core.Clock()
 # Randomizing the letters involved #
 ####################################
 
-def letterrandomizer():
-	numbertoletter=random.randint(1,26)
-	a="A"
-	b="B"
-	c="C"
-	d="D"
-	e="E"
-	f="F"
-	g="G"
-	h="H"
-	i="I"
-	j="J"
-	k="K"
-	l="L"
-	m="M"
-	n="N"
-	o="O"
-	p="P"
-	q="Q"
-	r="R"
-	s="S"
-	t="T"
-	u="U"
-	v="V"
-	w="W"
-	x="X"
-	y="Y"
-	z="Z"
-	numbertoletterperm=numbertoletter
-	if numbertoletterperm==1:
-		A=numbertoletter
-		return a
-	elif numbertoletterperm==2:
-		B=numbertoletter
-		return b
-	elif numbertoletter==3:
-		C=numbertoletter
-		return c
-	elif numbertoletter==4:
-		D=numbertoletter
-		return d
-	elif numbertoletter==5:
-		E=numbertoletter
-		return e
-	elif numbertoletter==6:
-		F=numbertoletter
-		return f
-	elif numbertoletter==7:
-		G=numbertoletter
-		return g
-	elif numbertoletter==8:
-		H=numbertoletter
-		return h
-	elif numbertoletter==9:
-		I=numbertoletter
-		return i
-	elif numbertoletter==10:
-		J=numbertoletter
-		return j
-	elif numbertoletter==11:
-		K=numbertoletter
-		return k
-	elif numbertoletter==12:
-		L=numbertoletter
-		return l
-	elif numbertoletter==13:
-		M=numbertoletter
-		return m
-	elif numbertoletter==14:
-		N=numbertoletter
-		return n
-	elif numbertoletter==15:
-		O=numbertoletter
-		return o
-	elif numbertoletter==16:
-		P=numbertoletter
-		return p
-	elif numbertoletter==17:
-		Q=numbertoletter
-		return q
-	elif numbertoletter==18:
-		R=numbertoletter
-		return r
-	elif numbertoletter==19:
-		S=numbertoletter
-		return s
-	elif numbertoletter==20:
-		T=numbertoletter
-		return t
-	elif numbertoletter==21:
-		U=numbertoletter
-		return u
-	elif numbertoletter==22:
-		V=numbertoletter
-		return v
-	elif numbertoletter==23:
-		W=numbertoletter
-		return w
-	elif numbertoletter==24:
-		X=numbertoletter
-		return x
-	elif numbertoletter==25:
-		Y=numbertoletter
-		return y
-	elif numbertoletter==26:
-		Z=numbertoletter
-		return z
+def randomletterarray1():
+	global outputarray
+	outputarray=[0]*8
+	for i in range(0,8):
+		numbertoletter=random.randint(1,26)
+		if numbertoletter==1:
+			outputarray[i]="A"
+		elif numbertoletter==2:
+			outputarray[i]="B"
+		elif numbertoletter==3:
+			outputarray[i]="C"
+		elif numbertoletter==4:
+			outputarray[i]="D"
+		elif numbertoletter==5:
+			outputarray[i]="E"
+		elif numbertoletter==6:
+			outputarray[i]="F"
+		elif numbertoletter==7:
+			outputarray[i]="G"
+		elif numbertoletter==8:
+			outputarray[i]="H"
+		elif numbertoletter==9:
+			outputarray[i]="I"
+		elif numbertoletter==10:
+			outputarray[i]="J"
+		elif numbertoletter==11:
+			outputarray[i]="K"
+		elif numbertoletter==12:
+			outputarray[i]="L"
+		elif numbertoletter==13:
+			outputarray[i]="M"
+		elif numbertoletter==14:
+			outputarray[i]="N"
+		elif numbertoletter==15:
+			outputarray[i]="O"
+		elif numbertoletter==16:
+			outputarray[i]="P"
+		elif numbertoletter==17:
+			outputarray[i]="Q"
+		elif numbertoletter==18:
+			outputarray[i]="R"
+		elif numbertoletter==19:
+			outputarray[i]="S"
+		elif numbertoletter==20:
+			outputarray[i]="T"
+		elif numbertoletter==21:
+			outputarray[i]="U"
+		elif numbertoletter==22:
+			outputarray[i]="V"
+		elif numbertoletter==23:
+			outputarray[i]="W"
+		elif numbertoletter==24:
+			outputarray[i]="X"
+		elif numbertoletter==25:
+			outputarray[i]="Y"
+		elif numbertoletter==26:
+			outputarray[i]="Z"
+		print outputarray[i]
 
 
+def randomletterarray2():
+	global outputarray2
+	outputarray2=[0]*8
+	for i in range(0,8):
+		numbertoletter=random.randint(1,26)
+		if numbertoletter==1:
+			outputarray2[i]="A"
+		elif numbertoletter==2:
+			outputarray2[i]="B"
+		elif numbertoletter==3:
+			outputarray2[i]="C"
+		elif numbertoletter==4:
+			outputarray2[i]="D"
+		elif numbertoletter==5:
+			outputarray2[i]="E"
+		elif numbertoletter==6:
+			outputarray2[i]="F"
+		elif numbertoletter==7:
+			outputarray2[i]="G"
+		elif numbertoletter==8:
+			outputarray2[i]="H"
+		elif numbertoletter==9:
+			outputarray2[i]="I"
+		elif numbertoletter==10:
+			outputarray2[i]="J"
+		elif numbertoletter==11:
+			outputarray2[i]="K"
+		elif numbertoletter==12:
+			outputarray2[i]="L"
+		elif numbertoletter==13:
+			outputarray2[i]="M"
+		elif numbertoletter==14:
+			outputarray2[i]="N"
+		elif numbertoletter==15:
+			outputarray2[i]="O"
+		elif numbertoletter==16:
+			outputarray2[i]="P"
+		elif numbertoletter==17:
+			outputarray2[i]="Q"
+		elif numbertoletter==18:
+			outputarray2[i]="R"
+		elif numbertoletter==19:
+			outputarray2[i]="S"
+		elif numbertoletter==20:
+			outputarray2[i]="T"
+		elif numbertoletter==21:
+			outputarray2[i]="U"
+		elif numbertoletter==22:
+			outputarray2[i]="V"
+		elif numbertoletter==23:
+			outputarray2[i]="W"
+		elif numbertoletter==24:
+			outputarray2[i]="X"
+		elif numbertoletter==25:
+			outputarray2[i]="Y"
+		elif numbertoletter==26:
+			outputarray2[i]="Z"
+		print outputarray2[i]
 
+def randomletterarray3():
+	global outputarray3
+	outputarray3=[0]*8
+	for i in range(0,8):
+		numbertoletter=random.randint(1,26)
+		if numbertoletter==1:
+			outputarray3[i]="A"
+		elif numbertoletter==2:
+			outputarray3[i]="B"
+		elif numbertoletter==3:
+			outputarray3[i]="C"
+		elif numbertoletter==4:
+			outputarray3[i]="D"
+		elif numbertoletter==5:
+			outputarray3[i]="E"
+		elif numbertoletter==6:
+			outputarray3[i]="F"
+		elif numbertoletter==7:
+			outputarray3[i]="G"
+		elif numbertoletter==8:
+			outputarray3[i]="H"
+		elif numbertoletter==9:
+			outputarray3[i]="I"
+		elif numbertoletter==10:
+			outputarray3[i]="J"
+		elif numbertoletter==11:
+			outputarray3[i]="K"
+		elif numbertoletter==12:
+			outputarray3[i]="L"
+		elif numbertoletter==13:
+			outputarray3[i]="M"
+		elif numbertoletter==14:
+			outputarray3[i]="N"
+		elif numbertoletter==15:
+			outputarray3[i]="O"
+		elif numbertoletter==16:
+			outputarray3[i]="P"
+		elif numbertoletter==17:
+			outputarray3[i]="Q"
+		elif numbertoletter==18:
+			outputarray3[i]="R"
+		elif numbertoletter==19:
+			outputarray3[i]="S"
+		elif numbertoletter==20:
+			outputarray3[i]="T"
+		elif numbertoletter==21:
+			outputarray3[i]="U"
+		elif numbertoletter==22:
+			outputarray3[i]="V"
+		elif numbertoletter==23:
+			outputarray3[i]="W"
+		elif numbertoletter==24:
+			outputarray3[i]="X"
+		elif numbertoletter==25:
+			outputarray3[i]="Y"
+		elif numbertoletter==26:
+			outputarray3[i]="Z"
+		print outputarray3[i]
 
+def randomletterarray4():
+	global outputarray4
+	outputarray4=[0]*8
+	for i in range(0,8):
+		numbertoletter=random.randint(1,26)
+		if numbertoletter==1:
+			outputarray4[i]="A"
+		elif numbertoletter==2:
+			outputarray4[i]="B"
+		elif numbertoletter==3:
+			outputarray4[i]="C"
+		elif numbertoletter==4:
+			outputarray4[i]="D"
+		elif numbertoletter==5:
+			outputarray4[i]="E"
+		elif numbertoletter==6:
+			outputarray4[i]="F"
+		elif numbertoletter==7:
+			outputarray4[i]="G"
+		elif numbertoletter==8:
+			outputarray4[i]="H"
+		elif numbertoletter==9:
+			outputarray4[i]="I"
+		elif numbertoletter==10:
+			outputarray4[i]="J"
+		elif numbertoletter==11:
+			outputarray4[i]="K"
+		elif numbertoletter==12:
+			outputarray4[i]="L"
+		elif numbertoletter==13:
+			outputarray4[i]="M"
+		elif numbertoletter==14:
+			outputarray4[i]="N"
+		elif numbertoletter==15:
+			outputarray4[i]="O"
+		elif numbertoletter==16:
+			outputarray4[i]="P"
+		elif numbertoletter==17:
+			outputarray4[i]="Q"
+		elif numbertoletter==18:
+			outputarray4[i]="R"
+		elif numbertoletter==19:
+			outputarray4[i]="S"
+		elif numbertoletter==20:
+			outputarray4[i]="T"
+		elif numbertoletter==21:
+			outputarray4[i]="U"
+		elif numbertoletter==22:
+			outputarray4[i]="V"
+		elif numbertoletter==23:
+			outputarray4[i]="W"
+		elif numbertoletter==24:
+			outputarray4[i]="X"
+		elif numbertoletter==25:
+			outputarray4[i]="Y"
+		elif numbertoletter==26:
+			outputarray4[i]="Z"
+		print outputarray4[i]
 
 def lettertext(text1, text2, text3, text4, text5, text6, text7, text8 ,h, posi):
 	one=visual.TextStim(win=window, text=text1, height=h, pos=(0.0, posi))
@@ -157,65 +282,41 @@ def lettertext(text1, text2, text3, text4, text5, text6, text7, text8 ,h, posi):
 	seven.draw()
 	eight.draw()
 
-Instructions1=visual.TextStim(win=window, text='Record the letters and position of the letters that you can recall, then press the spacebar when ready to see the actual configuration', pos=(0,0))
+Instructions1=visual.TextStim(win=window, text='Record the letters and position of the letters that you can recall, then get ready to see the actual configuration', pos=(0,0))
 
-def letterring():
-	lettertext("A","B","C","D","E","F","G","H",2.0,5.0)
-	lettertext("A","B","C","D","E","F","G","H",2.0,5.0)
-	lettertext("A","B","C","D","E","F","G","H",2.0,5.0)
-	lettertext("A","B","C","D","E","F","G","H",2.0,5.0)
+
 
 
 def letterfunc():
-	count=0
+	randomletterarray1()
+	randomletterarray2()
+	randomletterarray3()
+	randomletterarray4()
 	countspace=0
 	countinstruct=0
-	while clock.getTime()<=20.0:
-		print clock.getTime()
+	while clock.getTime()<=40.0:
+		#print clock.getTime()
 		if clock.getTime()<3.0:
 			vline.draw()
 			hline.draw()
 			window.flip()
 		elif event.getKeys("space"):
-			print "Fuck"
 			break
-		elif clock.getTime()>=5.0 and clock.getTime()<=10.0:
+		elif clock.getTime()>=5.0 and clock.getTime()<=15.0:
 			Instructions1.draw()
 			window.flip()
 		elif clock.getTime()>=3.0 and clock.getTime()<=5.0:
-			if count==0:
-				ring1=lettertext(letterrandomizer(),letterrandomizer(),letterrandomizer(),letterrandomizer(),letterrandomizer(),letterrandomizer(),letterrandomizer(),letterrandomizer(),1.0,1.0)
-				#lettertext("H","G","F","E","D","C","B","A",2.0,3.0)
-				#lettertext("A","B","C","D","E","F","G","H",3.0,5.0)
-				ring2=lettertext("A","B","C","D","E","F","G","H",2.0,5.0)
-				ring1perm=ring1
-				ring2perm=ring2
-
-				window.flip()
-				count=1
-			elif count>=1:
-				count=1 
-		elif clock.getTime()>=10.0 and clock.getTime()<20.0:
-			#if ring2perm==lettertext("A","B","C","D","E","F","G","H",2.0,5.0):
-				#print "Kill"
-			ring1()
-			ring2()
+			lettertext(outputarray[0],outputarray[1],outputarray[2],outputarray[3],outputarray[4],outputarray[5],outputarray[6],outputarray[7],1.0,1.0)
+			lettertext(outputarray2[0],outputarray2[1],outputarray2[2],outputarray2[3],outputarray2[4],outputarray2[5],outputarray2[6],outputarray2[7],2.0,4.0)
+			lettertext(outputarray3[0],outputarray3[1],outputarray3[2],outputarray3[3],outputarray3[4],outputarray3[5],outputarray3[6],outputarray3[7],3.0,7.0)
+			lettertext(outputarray4[0],outputarray4[1],outputarray4[2],outputarray4[3],outputarray4[4],outputarray4[5],outputarray4[6],outputarray4[7],4.0,10.0)
 			window.flip()
-
-				
-
-			#if clock.getTime()>=20.0 and clock.getTime()<=30.0:
-			#	clock.getTime()=11.0
-	#		elif event.getKeys('space'):
-
-
-
-
-
-
-
-
-
+		elif clock.getTime()>=15.0 and clock.getTime()<40.0:
+			lettertext(outputarray[0],outputarray[1],outputarray[2],outputarray[3],outputarray[4],outputarray[5],outputarray[6],outputarray[7],1.0,1.0)
+			lettertext(outputarray2[0],outputarray2[1],outputarray2[2],outputarray2[3],outputarray2[4],outputarray2[5],outputarray2[6],outputarray2[7],2.0,4.0)
+			lettertext(outputarray3[0],outputarray3[1],outputarray3[2],outputarray3[3],outputarray3[4],outputarray3[5],outputarray3[6],outputarray3[7],3.0,7.0)
+			lettertext(outputarray4[0],outputarray4[1],outputarray4[2],outputarray4[3],outputarray4[4],outputarray4[5],outputarray4[6],outputarray4[7],3.0,10.0)
+			window.flip()
 
 window.flip()
 clock.reset
